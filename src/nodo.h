9acc5e2f0651982e91e1284a186b0db57b65dc67
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2023-09-28 23:40:55 -0300

    Actualizar nodo.h

--- src/nodo.h
+++ src/nodo.h
@@ -10,8 +10,12 @@ typedef struct nodo {
 	struct nodo *siguiente;
 } nodo_t;
 
+//PRE: el nodo recibido es no nulo y la posición mayor a 0
+//POST: devuelve un puntero al nodo en la posición pedida
 nodo_t *posicion_nodo(nodo_t *nodo, int posicion);
 
+//PRE:-
+//POST: devuelve un puntero a un nodo inicializado con el elemento pasado por parametro
 nodo_t *nuevo_nodo(void *elemento);
 
 #endif /*__NODO_H_*/
